961103b51bdd6fa7420e7047a68d74f1275a3c73
Manh-Ha Vu <manhha.hut@gmail.com>
2013-07-14 15:21:41 +0200

    Remove duplicated function read-excel-file

--- src/clj_column/core.clj
+++ src/clj_column/core.clj
@@ -1,5 +1,5 @@
 (ns clj-column.core
-  (:import [org.apache.poi.ss.usermodel WorkbookFactory Workbook Sheet Row Cell]
+  (:import [org.apache.poi.ss.usermodel WorkbookFactory Workbook Sheet Row Cell DateUtil]
            [java.io.FileInputStream]))
 
 (defn read-excel-header [hdr]
@@ -16,27 +16,13 @@
           (cond
           (= cell-type Cell/CELL_TYPE_STRING) (.getString (.getRichStringCellValue cell))
           (= cell-type Cell/CELL_TYPE_BLANK) ""
-          (= cell-type Cell/CELL_TYPE_NUMERIC) (if (org.apache.poi.ss.usermodel.DateUtil/isCellDateFormatted cell) (.getDateCellValue cell) (.getNumericCellValue cell))
+          (= cell-type Cell/CELL_TYPE_NUMERIC) (if (DateUtil/isCellDateFormatted cell) (.getDateCellValue cell) (.getNumericCellValue cell))
           (= cell-type Cell/CELL_TYPE_BOOLEAN) (.getBooleanCellValue cell)
           (= cell-type Cell/CELL_TYPE_FORMULA) (.getCellFormula cell)
           (= cell-type Cell/CELL_TYPE_ERROR) nil)))
-
-(defn read-excel-file [path]
-  (let [excel (File. path)
-        fis (FileInputStream. excel)
-        wb (HSSFWorkbook. fis)
-        ws (.getSheetAt wb 0)
-        hdr (.getRow 0)]
-        
-  []))
 
 (defn read-excel-file [path]
   (let [wb (WorkbookFactory/create (FileInputStream. path))
         sh (.getSheetAt wb 0)]
     (map (fn [row]
            (map read-excel-cell row)) sh)))
-
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))

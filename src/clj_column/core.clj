999db00d562b9ada3b6678e18da3eac1ad8cfab1
Manh-Ha Vu <manhha.hut@gmail.com>
2013-07-15 20:14:16 +0200

    Fixed bug of reading row

--- src/clj_column/core.clj
+++ src/clj_column/core.clj
@@ -19,12 +19,31 @@
      (= cell-type Cell/CELL_TYPE_NUMERIC) (if (DateUtil/isCellDateFormatted cell) (.getDateCellValue cell) (.getNumericCellValue cell))
      (= cell-type Cell/CELL_TYPE_BOOLEAN) (.getBooleanCellValue cell)
      (= cell-type Cell/CELL_TYPE_FORMULA) (.getCellFormula cell)     
-     (= cell-type Cell/CELL_TYPE_ERROR) nil
-     nil)))
+     (= cell-type Cell/CELL_TYPE_ERROR) nil)))
+
+(defn read-excel-row[row]
+  (let [ncols (.getLastCellNum row)]
+    (vector 
+     (for [c (range 1 ncols)]
+       (read-excel-cell (.getCell row c Row/CREATE_NULL_AS_BLANK))))))
 
 (defn read-excel-file [path]
+  (println path)
   (with-open [in (FileInputStream. path)]
     (let [wb (WorkbookFactory/create in)
           sh (.getSheetAt wb 0)]
-      (map (fn [row]
-             (map read-excel-cell row)) sh))))
+      (for [r (range 1 (inc (.getLastRowNum sh)))]
+        (read-excel-row (.getRow sh r))))))
+
+
+
+
+
+
+
+
+
+
+
+
+

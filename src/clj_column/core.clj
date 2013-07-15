fbeb0e1affa16b12c52690b1d5cc29523ba396d0
Manh-Ha Vu <manhha.hut@gmail.com>
2013-07-15 22:21:48 +0200

    Working excel reader

--- src/clj_column/core.clj
+++ src/clj_column/core.clj
@@ -19,21 +19,23 @@
      (= cell-type Cell/CELL_TYPE_NUMERIC) (if (DateUtil/isCellDateFormatted cell) (.getDateCellValue cell) (.getNumericCellValue cell))
      (= cell-type Cell/CELL_TYPE_BOOLEAN) (.getBooleanCellValue cell)
      (= cell-type Cell/CELL_TYPE_FORMULA) (.getCellFormula cell)     
-     (= cell-type Cell/CELL_TYPE_ERROR) nil)))
+     (= cell-type Cell/CELL_TYPE_ERROR) "error")))
 
 (defn read-excel-row[row]
   (let [ncols (.getLastCellNum row)]
     (vector 
-     (for [c (range 1 ncols)]
+     (for [c (range 0 (inc ncols))]
        (read-excel-cell (.getCell row c Row/CREATE_NULL_AS_BLANK))))))
 
 (defn read-excel-file [path]
-  (println path)
   (with-open [in (FileInputStream. path)]
     (let [wb (WorkbookFactory/create in)
-          sh (.getSheetAt wb 0)]
-      (for [r (range 1 (inc (.getLastRowNum sh)))]
-        (read-excel-row (.getRow sh r))))))
+          sh (.getSheetAt wb 0)
+          header (read-excel-header (.getRow sh 0))]
+      (for [r (range 1 (inc (.getLastRowNum sh)))
+            row (read-excel-row (.getRow sh r))]
+          (zipmap header row)))))
+
 
 
 
--- test/clj_column/core_test.clj
+++ test/clj_column/core_test.clj
@@ -2,7 +2,7 @@
   (:use clojure.test
         clj-column.core))
 
-(def excel-path "test.xls")
+(def excel-path (.getPath (clojure.java.io/resource "test.xls")))
 
 (deftest read-lines-from-excel-file
   (let [lines (read-excel-file excel-path)]
@@ -10,14 +10,14 @@
       (is (= (count lines) 4)))
     (testing "First line must have"
       (let [line (nth lines 0)]
-        (is (= (:COL_1 line) 1))
+        (is (= (:COL_1 line) 1.0))
         (is (= (:COL_2 line) "Name 1"))
-        (is (= (:COL_3 line) (.parse (java.text.SimpleDateFormat "MM/dd/yyyy") "01/02/2014")))
+        (is (= (:COL_3 line) (.parse (java.text.SimpleDateFormat. "MM/dd/yyyy") "01/02/2014")))
         (is (= (:COL_4 line) 65.12))))
     (testing "Third line must have"
       (let [line (nth lines 2)]
-        (is (= (:COL_1 line) 3))
-        (is (= (:COL_2 line) "Name 2"))
-        (is (= (:COL_3 line) (.parse (java.text.SimpleDateFormat "MM/dd/yyyy") "12/30/2014")))
-        (is (= (:COL_4 line) 28))))))
+        (is (= (:COL_1 line) 3.0))
+        (is (= (:COL_2 line) "Name 3"))
+        (is (= (:COL_3 line) (.parse (java.text.SimpleDateFormat. "MM/dd/yyyy") "12/30/2014")))
+        (is (= (:COL_4 line) "12+16")))))) ; Formula
 
